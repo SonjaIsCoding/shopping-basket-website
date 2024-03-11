60c3c982b4969f2922f60d63bdc25bbcda5dcc33
Sonja Dahlke <sonja-dahlke@web.de>
2024-03-11 11:03:12 -0700

    add useEffect

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./Home.module.css";
 
 export function Home() {
   const [products, setProducts] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadProducts(params) {
+      console.log("Load Data");
+      try {
+        setIsLoading(true);
+        const response = await axios.get(
+          `${"https://prj-backend-shopping-basket.onrender.com/products"}/products`
+        );
+        console.log(response.data);
+        console.log(response.status);
+        setProducts(response.data);
+      } catch (err) {
+        setIsError(true);
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    loadProducts();
+  }, []);
+
   return (
     <>
-      <ul>
+      <header>
+        <h1 className={styles.rudi}>Rudis Resterampe</h1>
+      </header>
+      <>
+        {console.log(products)}
         {products.map((product) => {
-          <li>{product.title}</li>;
+          return (
+            <>
+              <div className={styles.title}>
+                <h1>{product.title}</h1>
+                <p>{product.category}</p>
+                <p>{product.description}</p>
+                <p>{product.price}</p>
+                <p>{product.stock}</p>
+              </div>
+            </>
+          );
         })}
-      </ul>
+      </>
     </>
   );
 }
